4a7420c60e4a5d518f39f3cb7e10e0af590bc203
ganurag6 <37958074+ganurag6@users.noreply.github.com>
2018-03-31 20:09:32 +0530

    Add files via upload

---
+++ Basics.py
@@ -0,0 +1,170 @@
+
+# coding: utf-8
+
+# In[3]:
+
+
+import pandas as pd
+import numpy as np
+
+cars = pd.read_csv('imports-85.data')
+
+
+# In[4]:
+
+
+cols = ['symboling', 'normalized-losses', 'make', 'fuel-type', 'aspiration', 'num-of-doors', 'body-style', 
+        'drive-wheels', 'engine-location', 'wheel-base', 'length', 'width', 'height', 'curb-weight', 'engine-type', 
+        'num-of-cylinders', 'engine-size', 'fuel-system', 'bore', 'stroke', 'compression-rate', 'horsepower', 'peak-rpm', 'city-mpg', 'highway-mpg', 'price']
+cars = pd.read_csv('imports-85.data', names=cols)
+cars.head()
+
+
+# In[7]:
+
+
+continuous_values_cols = ['normalized-losses', 'wheel-base', 'length', 'width', 'height', 'curb-weight', 'bore', 'stroke', 'compression-rate', 'horsepower', 'peak-rpm', 'city-mpg', 'highway-mpg', 'price']
+numeric_cars = cars[continuous_values_cols]
+numeric_cars
+
+
+# In[8]:
+
+
+numeric_cars = numeric_cars.replace('?', np.nan)
+numeric_cars.head(10)
+
+
+# In[10]:
+
+
+numeric_cars = numeric_cars.astype('float')
+numeric_cars = numeric_cars.dropna(subset = ['price'])
+numeric_cars.isnull().sum()
+
+
+# In[11]:
+
+
+numeric_cars = numeric_cars.fillna(numeric_cars.mean())
+numeric_cars.isnull().sum()
+
+
+# In[12]:
+
+
+price_col = numeric_cars['price']
+numeric_cars = (numeric_cars - numeric_cars.min())/(numeric_cars.max() - numeric_cars.min())
+numeric_cars['price'] = price_col
+
+
+# In[ ]:
+
+
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.metrics import mean_squared_error
+
+def knn_train_test(train_col, target_col, df):
+    knn = KNeighborsRegressor()
+    np.random.seed(1)
+        
+    # Randomize order of rows in data frame.
+    shuffled_index = np.random.permutation(df.index)
+    rand_df = df.reindex(shuffled_index)
+
+    # Divide number of rows in half and round.
+    last_train_row = int(len(rand_df) / 2)
+    
+    # Select the first half and set as training set.
+    # Select the second half and set as test set.
+    train_df = rand_df.iloc[0:last_train_row]
+    test_df = rand_df.iloc[last_train_row:]
+    
+    # Fit a KNN model using default k value.
+    knn.fit(train_df[[train_col]], train_df[target_col])
+    
+    # Make predictions using model.
+    predicted_labels = knn.predict(test_df[[train_col]])
+
+    # Calculate and return RMSE.
+    mse = mean_squared_error(test_df[target_col], predicted_labels)
+    rmse = np.sqrt(mse)
+    return rmse
+
+rmse_results = {}
+train_cols = numeric_cars.columns.drop('price')
+
+# For each column (minus `price`), train a model, return RMSE value
+# and add to the dictionary `rmse_results`.
+for col in train_cols:
+    rmse_val = knn_train_test(col, 'price', numeric_cars)
+    rmse_results[col] = rmse_val
+
+# Create a Series object from the dictionary so 
+# we can easily view the results, sort, etc
+rmse_results_series = pd.Series(rmse_results)
+rmse_results_series.sort_values()
+
+
+# In[ ]:
+
+
+def knn_train_test(train_col, target_col, df):
+    np.random.seed(1)
+        
+    # Randomize order of rows in data frame.
+    shuffled_index = np.random.permutation(df.index)
+    rand_df = df.reindex(shuffled_index)
+
+    # Divide number of rows in half and round.
+    last_train_row = int(len(rand_df) / 2)
+    
+    # Select the first half and set as training set.
+    # Select the second half and set as test set.
+    train_df = rand_df.iloc[0:last_train_row]
+    test_df = rand_df.iloc[last_train_row:]
+    
+    k_values = [1,3,5,7,9]
+    k_rmses = {}
+    
+    for k in k_values:
+        # Fit model using k nearest neighbors.
+        knn = KNeighborsRegressor(n_neighbors=k)
+        knn.fit(train_df[[train_col]], train_df[target_col])
+
+        # Make predictions using model.
+        predicted_labels = knn.predict(test_df[[train_col]])
+
+        # Calculate and return RMSE.
+        mse = mean_squared_error(test_df[target_col], predicted_labels)
+        rmse = np.sqrt(mse)
+        
+        k_rmses[k] = rmse
+    return k_rmses
+
+k_rmse_results = {}
+
+# For each column (minus `price`), train a model, return RMSE value
+# and add to the dictionary `rmse_results`.
+train_cols = numeric_cars.columns.drop('price')
+for col in train_cols:
+    rmse_val = knn_train_test(col, 'price', numeric_cars)
+    k_rmse_results[col] = rmse_val
+
+k_rmse_results
+
+
+# In[ ]:
+
+
+import matplotlib.pyplot as plt
+get_ipython().magic('matplotlib inline')
+
+for k,v in k_rmse_results.items():
+    x = list(v.keys())
+    y = list(v.values())
+    
+    plt.plot(x,y)
+    plt.xlabel('k value')
+    plt.ylabel('RMSE')
+
